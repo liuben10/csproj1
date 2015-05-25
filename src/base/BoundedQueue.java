2b3cf3b5ad637197d4a7e39277480a36d39c004f
liuben10 <liuben10@192.168.1.144>
2015-05-24 22:13:50 -0700

    Add generated BoundedQueue

---
+++ src/base/BoundedQueue.java
@@ -0,0 +1,53 @@
+package base;
+
+public class BoundedQueue implements InstructuresDeque {
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void addTop(Object element) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void addBottom(Object element) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Object removeTop() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object removeBottom() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object top() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object bottom() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
