eca56a1839b7ec35c808725e3ed26c0dc3a0e160
liuben10 <liuben10@192.168.1.144>
2015-05-24 22:53:09 -0700

    BoundedQueue Implementation.

--- src/base/BoundedQueue.java
+++ src/base/BoundedQueue.java
@@ -1,53 +1,121 @@
 package base;
 
 public class BoundedQueue implements InstructuresDeque {
+	
+	public static void main(String...args) {
+		BoundedQueue bq = new BoundedQueue(10);
+		bq.addTop("foo");
+		bq.addTop(1);
+		bq.addTop(2);
+		bq.addBottom(3);
+		bq.addBottom(4);
+		System.out.println(bq.forwardString());
+	}
+	
+	int capacity;
+	
+	int size;
+	
+	Object[] data;
+	
+	int head = -1;
+	int tail = -1;
+	
+	BoundedQueue(int capacity) {
+		this.capacity = capacity;
+		this.data = new Object[capacity];
+	}
 
 	@Override
 	public int size() {
-		// TODO Auto-generated method stub
-		return 0;
+		return size;
 	}
 
 	@Override
 	public boolean isEmpty() {
-		// TODO Auto-generated method stub
-		return false;
+		return size == 0;
 	}
 
 	@Override
 	public void addTop(Object element) {
-		// TODO Auto-generated method stub
-		
+		if (head == -1) {
+			head = 0;
+			tail = 0;
+		} else {
+			head = (head + capacity - 1) % capacity;
+		}
+		data[head] = element;
+		size += 1;
 	}
 
 	@Override
 	public void addBottom(Object element) {
-		// TODO Auto-generated method stub
-		
+		if (tail == -1) {
+			tail = 0;
+			head = 0;
+		} else {
+			tail = (tail + 1) % capacity;
+		}
+		data[tail] = element;
+		size += 1;
+	}
+	
+	public String forwardString() {
+		int ptr = head;
+		StringBuilder sb = new StringBuilder();
+		for(int i = 0; i < size; i++) {
+			sb.append(data[ptr] + ", ");
+			ptr = (ptr + 1) % capacity;
+		}
+		return sb.toString();
+	}
+	
+	public String backwardString() {
+		int ptr = tail;
+		StringBuilder sb = new StringBuilder();
+		for(int i = 0; i < size; i++) {
+			sb.append(data[ptr] + ", ");
+			ptr = (ptr + capacity - 1) % capacity;
+		}
+		return sb.toString();
 	}
 
 	@Override
 	public Object removeTop() {
-		// TODO Auto-generated method stub
-		return null;
+		if (isEmpty()) {
+			throw new IllegalArgumentException("Attempted to remove top from empty bounded queue");
+		}
+		Object element = data[head];
+		head = (head + 1) % capacity;
+		size -= 1;
+		return element;
 	}
 
 	@Override
 	public Object removeBottom() {
-		// TODO Auto-generated method stub
-		return null;
+		if (isEmpty()) {
+			throw new IllegalArgumentException("Attempted to remove top from empty bounded queue");
+		}
+		Object element = data[tail];
+		tail = (tail + capacity - 1) % capacity;
+		size -= 1;
+		return element;
 	}
 
 	@Override
 	public Object top() {
-		// TODO Auto-generated method stub
-		return null;
+		if (isEmpty()) {
+			return null;
+		}
+		return data[head];
 	}
 
 	@Override
 	public Object bottom() {
-		// TODO Auto-generated method stub
-		return null;
+		if (isEmpty()) {
+			return null;
+		}
+		return data[tail];
 	}
 
 }
